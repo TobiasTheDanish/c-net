239f25cd76268d3b7153b92d46e7ed41713a6ff2
THCSC <tobias@skancode.dk>
2024-08-28 22:19:17 +0200

    start using handlers for endpoints

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include "include/http.h"
+#include <stdio.h>
+
+Response apiHandler(Request *);
 
 int main() {
   Server *s = Server_new(8080);
+
+  Server_addHandler(s, POST, "/api", apiHandler);
+
   Server_serve(s);
   return 0;
+}
+
+Response apiHandler(Request *req) {
+  printf("Request body:\n %s\n", req->body);
+
+  return Response_text(StatusOk, "Thanks for calling\n");
 }
