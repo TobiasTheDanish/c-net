90383a58c469ba68081145bce0dccf10d8c9d8b2
THCSC <tobias@skancode.dk>
2024-08-31 23:02:49 +0200

    add favicon and favicon handler

--- src/main.c
+++ src/main.c
@@ -4,11 +4,13 @@
 
 Response apiHandler(Request *);
 Response indexHandler(Request *req);
+Response faviconHandler(Request *req);
 
 int main() {
   Server *s = Server_new(8080);
 
   Server_addHandler(s, GET, "/", indexHandler);
+  Server_addHandler(s, GET, "/favicon.ico", faviconHandler);
   Server_addHandler(s, POST, "/api", apiHandler);
 
   Server_serve(s);
@@ -17,6 +19,10 @@ int main() {
 
 Response indexHandler(Request *req) {
   return Response_file(StatusOk, "text/html", "index.html");
+}
+
+Response faviconHandler(Request *req) {
+  return Response_file(StatusOk, "image/vnd.microsoft.icon", "favicon.ico");
 }
 
 Response apiHandler(Request *req) {
