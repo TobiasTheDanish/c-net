b0d6d89a155b4645223a1bf0a8bb8b0c43c57af4
THCSC <tobias@skancode.dk>
2024-09-12 23:21:41 +0200

    use proper file response creaters

--- src/main.c
+++ src/main.c
@@ -5,12 +5,14 @@
 Response apiHandler(Request *);
 Response indexHandler(Request *req);
 Response faviconHandler(Request *req);
+Response routerHandler(Request *req);
 
 int main() {
   Server *s = Server_new(8080);
 
   Server_addHandler(s, GET, "/", indexHandler);
   Server_addHandler(s, GET, "/favicon.ico", faviconHandler);
+  Server_addHandler(s, GET, "/router.png", routerHandler);
   Server_addHandler(s, POST, "/api", apiHandler);
 
   Server_serve(s);
@@ -18,11 +20,15 @@ int main() {
 }
 
 Response indexHandler(Request *req) {
-  return Response_file(StatusOk, "text/html", "index.html");
+  return Response_textFile(StatusOk, "text/html", "index.html");
+}
+
+Response routerHandler(Request *req) {
+  return Response_binaryFile(StatusOk, "image/png", "router.png");
 }
 
 Response faviconHandler(Request *req) {
-  return Response_file(StatusOk, "image/png", "favicon.png");
+  return Response_binaryFile(StatusOk, "image/png", "favicon.png");
 }
 
 Response apiHandler(Request *req) {
